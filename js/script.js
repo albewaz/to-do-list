fbee163b38be124953bf1c13eb01f80b736bbd6a
Albert Waz <albewaz@gmail.com>
2023-05-08 13:20:49 +0200

    function adding

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 {
+
     const tasks = [
         {
             content: "nagrać lekcję",
@@ -10,51 +11,95 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li
-                    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
-                >
-                    ${task.content}    
-                </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
-    }
+
+        const newTaskInput = document.querySelector(".js-newTask");
+        newTaskInput.value = "";
+        newTaskInput.focus();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li
+                ${task.done ? 'style="text-decoration: line-through"' : ""}
+            >
+                <button class="js-done">zrobione?</button>
+                <button class="js-remove">usuń</button>
+                ${task.content}    
+            </li>
+        `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+    };
 
     const onFormSubmit = (event) => {
-        event.preventDefault()
+        event.preventDefault();
 
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
-        console.log(newTaskContent);
+        const newTaskInput = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskInput.value.trim();
 
         if (newTaskContent === "") {
+            newTaskInput.focus();
             return;
         }
 
         addNewTask(newTaskContent);
     };
 
-
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
+        const newTaskInput = document.querySelector(".js-newTask");
+        const addTaskButton = document.querySelector(".js-form button[type='submit']");
 
         form.addEventListener("submit", onFormSubmit);
+        addTaskButton.addEventListener("click", () => {
+            if (newTaskInput.value === "") {
+                newTaskInput.focus();
+            }
+        });
     };
 
-    init()
+    init();
+
 }
